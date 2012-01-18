58b10aef668ebf50d40b8f8db3ece561ecf64b2e
landless.landman <landless.landman@gmail.com>
2012-01-18 06:05:50 +0000

    Add license declaration. Update piao.js.

--- plugin/npp_entry.cc
+++ plugin/npp_entry.cc
@@ -1,11 +1,28 @@
+/*
+ *  12306 Booking Helper Extension for Google Chrome and Chrome-base browsers such as 360chrome.
+ *  Copyright (C) 2012 Landman
+ * 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
 #include "stdafx.h"
 
-#include "log.h"
 #include "npfunctions.h"
 #include "plugin_base.h"
 #include "plugin_factory.h"
 
-extern Log g_Log;
+//extern Log g_Log;
 
 #ifdef XP_UNIX
 char* NPP_GetMIMEDescription() {
